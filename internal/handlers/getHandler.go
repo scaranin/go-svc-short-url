569a99ffc5d4585ddda255370ba84f1f61ab04f8
scaranin <amurmax@bk.ru>
2025-08-31 22:04:17 +0300

    iter19 commit9 Правки по результатам ревью

--- internal/handlers/getHandler.go
+++ internal/handlers/getHandler.go
@@ -54,13 +54,11 @@ func (h *URLHandler) GetUserURLs(w http.ResponseWriter, r *http.Request) {
 	)
 	cookieR, err := r.Cookie(h.Auth.CookieName)
 	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte(err.Error()))
-		return
+		log.Print(err.Error())
 	}
 	cookieW, err = h.Auth.FillUserReturnCookie(cookieR)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err.Error())
 	}
 	if err == http.ErrNoCookie {
 		w.WriteHeader(http.StatusNoContent)
